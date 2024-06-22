22bade8f40504f7c1b6a6a783b9d265cf3fa24a7
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-06-22 15:19:15 +0200

    Created an array for images

---
+++ app.js
@@ -0,0 +1,18 @@
+let cards = [
+    { name: "Object1", image: "path/to/cardA.JPG" },
+    { name: "Object2", image: "path/to/cardA.JPG" },
+    { name: "Object1", image: "path/to/cardB.JPG" },
+    { name: "Object2", image: "path/to/cardB.JPG" },
+    { name: "Object1", image: "path/to/cardC.JPG" },
+    { name: "Object2", image: "path/to/cardC.JPG" },
+    { name: "Object1", image: "path/to/cardD.JPG" },
+    { name: "Object2", image: "path/to/cardD.JPG" },
+    { name: "Object1", image: "path/to/cardE.JPG" },
+    { name: "Object2", image: "path/to/cardE.JPG" },
+    { name: "Object1", image: "path/to/cardF.JPG" },
+    { name: "Object2", image: "path/to/cardF.JPG" },
+    { name: "Object1", image: "path/to/cardG.JPG" },
+    { name: "Object2", image: "path/to/cardG.JPG" },
+    { name: "Object1", image: "path/to/cardH.JPG" },
+    { name: "Object2", image: "path/to/cardH.JPG" },
+  ];
